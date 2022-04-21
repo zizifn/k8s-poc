8798c8814acce94e7f76d43beafcaeabcb5a567e
zizifn <515868058@qq.com>
2022-04-21 21:28:12 +0800

    init terraform

--- oci-infra/test.tf
+++ oci-infra/test.tf
@@ -2,7 +2,7 @@
 provider "oci" {
     tenancy_ocid = var.tenancy_ocid
     user_ocid = var.user_ocid
-    private_key_path = var.private_key
+    private_key = var.private_key
     fingerprint = var.fingerprint
     region = var.region
 }
