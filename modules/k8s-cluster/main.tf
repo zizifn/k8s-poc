f913a904c8fbaa498fbaf359fb7ae22fdd381e35
zizfn <1803942+zizifn@users.noreply.github.com>
2022-04-25 06:15:58 +0800

    add oke and ingress nginx

--- modules/k8s-cluster/main.tf
+++ modules/k8s-cluster/main.tf
@@ -1,4 +1,11 @@
-
+terraform {
+  required_providers {
+    oci = {
+      source  = "oracle/oci"
+      version = "~> 4.72.0"
+    }
+  }
+}
 data "oci_core_vcn" "k8s_vcn" {
   #Required
   vcn_id = var.vcn_id
