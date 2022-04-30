1e328311921e5dadeddc0a88cc3be476e370ea20
zizfn <1803942+zizifn@users.noreply.github.com>
2022-04-30 22:34:27 +0800

    enhance module

--- k8s-infra/outputs.tf
+++ k8s-infra/outputs.tf
@@ -2,3 +2,8 @@ output "ingrss_nginx_lb_ip" {
   value = module.k8s-vcn-infra.ingrss_nginx_lb_ip
   sensitive = true
 }
+
+output "nsg_common_internet_access_id" {
+  value = module.k8s-vcn-infra.nsg_common_internet_access.id
+  sensitive = true
+}
