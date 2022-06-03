55a41925ea88d8c1edb2b71ef52c858e24371ce3
zizifn <515868058@qq.com>
2022-06-03 18:25:49 +0800

    add opentelmetry

--- k8s-base-apps/modules/k8s-opentelemetry-collector/main.tf
+++ k8s-base-apps/modules/k8s-opentelemetry-collector/main.tf
@@ -1,34 +1,64 @@
-resource "kubernetes_manifest" "opentelemetrycollector_otelcontribcol" {
-  manifest = {
-    "apiVersion" = "opentelemetry.io/v1alpha1"
-    "kind" = "OpenTelemetryCollector"
-    "metadata" = {
-      "name" = "otelcontribcol"
-      "namespace" = "default"
-    }
-    "spec" = {
-      "config" = <<-EOT
-      receivers:
-        otlp:
-          protocols:
-            grpc:
-            http:
-          k8s_cluster:
-            collection_interval: 10s
-      processors:
+resource "kubernetes_config_map_v1" "opentelemetry_k8s" {
+  metadata {
+    name = "opentelemetry_collector_k8s"
+  }
 
-      exporters:
-        logging:
+  data = {
+    "opentelemetry_k8s.yml" = "${file("${path.module}/data/opentelemetry_k8s.yml")}"
+  }
 
-      service:
-        pipelines:
-          traces:
-            receivers: [otlp, k8s_cluster]
-            processors: []
-            exporters: [logging]
+}
 
-      EOT
-      "serviceAccount" = "otelcontribcol-sa"
+resource "kubernetes_deployment" "otelcontribcol_k8s" {
+  metadata {
+    name = "otelcontribcol_k8s"
+
+    labels = {
+      app = "otelcontribcol_k8s"
     }
   }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = "otelcontribcol_k8s"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "otelcontribcol_k8s"
+        }
+      }
+
+      spec {
+        volume {
+          name = "config"
+
+          config_map {
+            name = "opentelemetry_collector_k8s"
+          }
+        }
+
+        container {
+          name  = "otelcontribcol"
+          image = "otel/opentelemetry-collector-contrib:latest"
+          args  = ["--config", "/etc/config/opentelemetry_k8s.yaml"]
+
+          volume_mount {
+            name       = "config"
+            mount_path = "/etc/config"
+          }
+        }
+        service_account_name = "otelcontribcol-sa"
+      }
+    }
+  }
+
+  depends_on = [
+    kubernetes_config_map_v1.opentelemetry_k8s
+  ]
 }
