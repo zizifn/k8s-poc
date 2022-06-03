1658fb2c163fab0a44b1c4ec7243b1fbd085bf8f
root <root@meta-unicorns-3.localdomain>
2022-06-03 09:44:09 +0000

    change to base64

--- k8s-infra/main.tf
+++ k8s-infra/main.tf
@@ -38,7 +38,7 @@ terraform {
 provider "oci" {
   tenancy_ocid = var.tenancy_ocid
   user_ocid    = var.user_ocid
-  private_key  = var.private_key
+  private_key  = try(base64decode(var.private_key), null)
   fingerprint  = var.fingerprint
   region       = var.region
 }
